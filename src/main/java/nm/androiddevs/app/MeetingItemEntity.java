3be8a40d461830ec130da06cec9965b0726d8b9b
Travis Decker <decker4693@gmail.com>
2019-04-22 21:39:50 -0600

    Created News Item Entity with some fields.

---
+++ src/main/java/nm/androiddevs/app/MeetingItemEntity.java
@@ -0,0 +1,13 @@
+package nm.androiddevs.app;
+
+public class MeetingItemEntity {
+
+  private String date;
+  private String time;
+  private String location;
+  private String description;
+  private String newsItem;
+  private String postingMember;
+  private String meetUpsLink;
+
+}
