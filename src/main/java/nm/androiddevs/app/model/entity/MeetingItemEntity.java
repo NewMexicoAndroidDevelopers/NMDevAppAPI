d72b4d0f65cb6453e0089114d6717c1d19d875f8
Travis Decker <decker4693@gmail.com>
2019-05-23 18:58:52 -0600

    Added Id to meeting and dInject

--- src/main/java/nm/androiddevs/app/model/entity/MeetingItemEntity.java
+++ src/main/java/nm/androiddevs/app/model/entity/MeetingItemEntity.java
@@ -1,9 +1,18 @@
 package nm.androiddevs.app.model.entity;
 
 
+import java.net.URI;
+import java.util.UUID;
+import javax.annotation.PostConstruct;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Getter
@@ -12,12 +21,31 @@ import org.springframework.stereotype.Component;
 @Component
 public class MeetingItemEntity {
 
-  private String date;
-  private String time;
-  private String location;
-  private String description;
-  private String newsItem;
-  private String postingMember;
-  private String meetUpsLink;
+  private static EntityLinks entityLinks;
+  @Id
+  @NonNull
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID meetingId;
+  private String meetingDate;
+  private String meetingTime;
+  private String meetingLocation;
+  private String meetingDescription;
+  private String MeeitngNewsItem;
+  private String meetingPostingMember;
+  private String meetingMeetUpsLink;
 
+  @PostConstruct
+  private void initEntityLinks() {
+    String ignore = entityLinks.toString();
+  }
+
+  @Autowired
+  private void setEntityLinks(EntityLinks entityLinks) {
+    MeetingItemEntity.entityLinks = entityLinks;
+  }
+
+  public URI getHref() {
+    return entityLinks.linkForSingleResource(MeetingItemEntity.class, meetingId).toUri();
+
+  }
 }
